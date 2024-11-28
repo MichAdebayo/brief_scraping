c687e5976b30a64350650243af0f6422563f9d92
David <david.scott.2875@gmail.com~>
2024-11-28 16:13:47 +0100

    sql

---
+++ castorama/Display.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib
+
+dfcat = pd.read_csv('categories.csv')
+dfprod = pd.read_csv('products.csv')
+price_prod_jardin = dfprod[['price', 'title']][dfprod['category'] == 'jardin-et-terrasse']
+max_price_cat = dfprod['category'][]
+# print(price_prod_jardin)
+print(dfprod.info())
